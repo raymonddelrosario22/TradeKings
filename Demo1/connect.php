75781f1dcdcdee60e1155ba65f0c8c93984e591b
Bhargav Sonani <sonanibhargav@gmail.com>
2018-04-02 00:20:33 -0400

    deleting Demo1

--- Demo1/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = 'userregistration.ccfiwisnnhob.us-east-2.rds.amazonaws.com';
-$connect=mysqli_connect($host,'admin','marsic123','UserRegistrationDB')or die("Couldn't Connect");
-
-//mysqli_select_db($connect,"forum")or die("Couldn't Connect");
-
-?>
